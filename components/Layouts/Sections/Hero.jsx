a163b04aa1f88042719cc2bdf63289aaf6828367
SmallDevStudio <smalldevstudio@gmail.com>
2025-02-22 00:21:32 +0700

    update

--- components/Layouts/Sections/Hero.jsx
+++ components/Layouts/Sections/Hero.jsx
@@ -26,8 +26,8 @@ export default function Hero() {
     return (
         <section className="bg-white dark:bg-gray-800">
              <div className="py-4 px-4 w-full lg:py-6 lg:px-6">
-                <div className="mx-auto max-w-screen-sm text-center lg:mb-1 mb-4">
-                    <h2 className="text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-orange-500">รู้จัก อ. เอ๋ อภัยลักษ์ ตันตระบัณฑิตย์</h2>
+                <div className="mx-auto max-w-screen-sm text-center lg:mb-1 ">
+                    <h2 className="text-2xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-orange-500">รู้จัก อ. เอ๋ อภัยลักษ์ ตันตระบัณฑิตย์</h2>
                         {/*<p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">We use an agile approach to test assumptions and connect with the needs of your audience early and often.</p>*/}
                 </div> 
             </div>
