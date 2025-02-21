819fa2e8f0494234f60ae0ee4c8a045c4c4fe8c3
SmallDevStudio <smalldevstudio@gmail.com>
2025-02-21 23:31:28 +0700

    update

--- components/Layouts/Sections/Hero.jsx
+++ components/Layouts/Sections/Hero.jsx
@@ -32,7 +32,7 @@ export default function Hero() {
                 </div> 
             </div>
             <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-2">
-                <div className="mr-aut lg:mt-0 lg:col-span-5 lg:flex ">  
+                <div className="mr-auto lg:mt-0 lg:col-span-5 lg:flex ">  
                     <Image 
                         src={heroContent.image} 
                         alt="mockup"
