26e3812260a44298bcfe4e7752bdfde922ca88c0
SmallDevStudio <smalldevstudio@gmail.com>
2025-03-17 09:38:40 +0700

    update

--- templates/default/Home.jsx
+++ templates/default/Home.jsx
@@ -13,17 +13,24 @@ import Courses from "@/components/Layouts/Sections/Courses";
 import NewsLetter from "@/components/Layouts/Sections/NewsLetter";
 
 export default function HomeTemplate({ title, content }) {
-    return (
-        <main className="flex-1 bg-white dark:bg-gray-800 text-black dark:text-gray-50 flex-col min-h-screen">
-            <div className="flex flex-col justify-between h-screen">
-                <div className="flex flex-col bg-white dark:bg-gray-800 text-black dark:text-gray-50 gap-2 w-full">
-                    <Header />
-                    {/* Sections */}
-                   
-                    <NewsLetter />
-                </div>
-                <Footer />
-            </div>
-        </main>
-    );
+  return (
+    <main className="flex-1 bg-white dark:bg-gray-800 text-black dark:text-gray-50 flex-col min-h-screen">
+      <div className="flex flex-col justify-between h-screen">
+        <div className="flex flex-col bg-white dark:bg-gray-800 text-black dark:text-gray-50 gap-2 w-full">
+          <Header />
+          {/* Sections */}
+          <Header />
+          {/* Sections */}
+          <Carousel />
+          <Hero />
+          <Blockquote />
+          <Team />
+          <Courses />
+          <Trainings />
+          <Partner />
+        </div>
+        <Footer />
+      </div>
+    </main>
+  );
 }
