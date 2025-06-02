8d095a49d811ac16169637d1a5324dc3cc21b5ec
SmallDevStudio <smalldevstudio@gmail.com>
2025-06-02 13:52:38 +0700

    custom

--- sections/Forms/FormCustoms.jsx
+++ sections/Forms/FormCustoms.jsx
@@ -28,9 +28,6 @@ export default function FormCustoms({
     setContents((prev) => ({ ...prev, [lang]: content }));
   };
 
-  console.log("contents", contents);
-  console.log("language", language);
-
   return (
     <div className="w-full">
       {/* Edit Mode */}
